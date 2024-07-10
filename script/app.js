9d7bc3edb4b509709ee733970212336b6d879b52
allan <4lwachira@gmail.com>
2024-07-10 16:18:56 +0300

    1  Add  utility functions 2 AddDOMcontentloaded event listener

---
+++ script/app.js
@@ -0,0 +1,35 @@
+//creates an element
+function createAnElement(element) {
+  return document.createElement(element);
+}
+//adds text
+function addText(element, text) {
+  return (element.innerText = text);
+}
+// appendChild
+function appendChild(child, parent) {
+  return parent.appendChild(child);
+}
+function select(selector) {
+  return document.querySelector(selector);
+}
+function listen(element, event, callback) {
+  return element.addEventListener(event, callback);
+}
+function addAttribute(element, attribute, content) {
+  return element.setAttribute(attribute, content);
+}
+const shoppingList = ["milk", "Tea", "bread"];
+
+const ol = select("ol");
+
+listen(document, "DOMContentLoaded", displayItems);
+
+function displayItems() {
+  shoppingList.forEach(createAListItem);
+}
+function createAListItem(item) {
+  const li = createAnElement("li");
+  addText(li, item);
+  appendChild(li, ol);
+}
